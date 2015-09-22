cd92e695596ef80b3814e0ec0129cef29a0e79be
hqingwang <2812085413@qq.com>
2015-09-22 12:33:04 +0800

    YYYY

---
+++ test.php
@@ -0,0 +1,10 @@
+<?php
+echo "HEllo World!";
+//我是王华栋，这仅仅是一个测试。
+/*
+
+在github上注册账号     hqingwang
+配置好ssh key          ok
+创建一个代码库随便提交一些代码,在这个代码库上创建一个test分支
+最后提供代码库访问的 ssl       https://github.com/goodren/shuideshui.git
+*/
